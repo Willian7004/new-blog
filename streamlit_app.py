904e3b9f2abfc9536c288dd820f4fcddcefc03ab
Willian7004 <128359604+Willian7004@users.noreply.github.com>
2025-05-06 16:11:30 +0800

    add files

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ with st.expander("项目说明"):
         content = f.read()
         st.markdown(content)
 st.sidebar.title("主页")
-st.sidebar.write("本页面显示项目说明和最近更新的内容。最近更新的内容链接指向GitHub对应文件，也可以直接展开全文。")
+st.sidebar.write("本页面显示项目说明和最近更新的内容。最近更新的内容包括创建和修改的文章，按修改时间从新到旧显示，可以直接展开全文。")
 
 st.subheader("最近更新：",divider=True)
 
